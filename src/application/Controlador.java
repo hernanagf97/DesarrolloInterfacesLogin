04e02978b67cc21e33e9ae23bc9adcea4e2854cb
hernan <hernanagf97@gmail.com>
2018-10-31 20:07:22 +0100

    push

--- src/application/Controlador.java
+++ src/application/Controlador.java
@@ -5,28 +5,37 @@ import java.io.IOException;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
-public class Boton {
+public class Controlador {
 	private AnchorPane mypane;
-	
+	@FXML
+	private Label cerrarVentana;
 	@FXML
 	public void crearCuenta() {
+		
+		
 		FXMLLoader loader = new FXMLLoader(getClass().getResource("CreateAccount.fxml"));
-
+		
 		Stage secondaryStage = new Stage();
 
+		Stage thisStage = (Stage) cerrarVentana.getScene().getWindow();
+		thisStage.close();
+		
 		try {
 			mypane = (AnchorPane) loader.load();
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
+				
 		secondaryStage.setTitle("Probando");
 		secondaryStage.setScene(new Scene(mypane));
 		secondaryStage.show();
+		
+		
 	}
 
 }
