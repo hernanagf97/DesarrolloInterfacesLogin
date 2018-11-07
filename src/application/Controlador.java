af28379073f4245e874c688b1ba84560a2b7e097
hernan <hernanagf97@gmail.com>
2018-11-07 16:24:29 +0100

    cc

--- src/application/Controlador.java
+++ src/application/Controlador.java
@@ -8,7 +8,7 @@ import javafx.scene.Scene;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
-public class Boton {
+public class Controlador {
 	private AnchorPane mypane;
 	
 	@FXML
